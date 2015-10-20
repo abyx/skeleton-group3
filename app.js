06f85764485b732e2b1eafb5c969694be08c2ab4
Aviv Ben-Yosef <aviv.by@gmail.com>
2015-10-20 11:51:47 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,132 @@
+var Q = require('q');
+var express = require('express');
+var bodyParser = require('body-parser');
+var elasticsearch = require('elasticsearch');
+var _ = require('lodash');
+var app = express();
+
+var client = new elasticsearch.Client({ host: 'localhost:9200', log: 'trace', apiVersion: '2.0' });
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.param('id', function(req, res, next) {
+  next();
+});
+
+app.get('/example', function(request, response) {
+  response.send({success: true});
+});
+
+app.post('/example/:id', function(request, response) {
+  console.log(request.body, request.params.id, 'query', request.query);
+  response.sendStatus(200);
+});
+
+app.post('/another/example', function(request, response) {
+  response.redirect('/example');
+});
+
+function resultToJson(result) {
+  return _.merge({id: result._id}, result._source);
+}
+
+app.route('/resources')
+  .get(function(request, response) {
+    client.search({
+      index: 'myindex',
+      type: 'resources'
+    }).then(
+      function(resources) {
+        response.send(_.map(resources.hits.hits, resultToJson));
+      },
+      function() {
+        response.sendStatus(500);
+      }
+    );
+  })
+  .post(function(request, response) {
+    client.create({
+      index: 'myindex',
+      type: 'resources',
+      body: request.body
+    }).then(function(result) {
+      return getResourceById(result._id).then(function(object) {
+        response.send(object);
+      });
+    }).catch(function() {
+      response.sendStatus(500);
+    });
+  });
+
+function getResourceById(id) {
+  return client.get({
+    index: 'myindex',
+    type: 'resources',
+    id: id
+  }).then(function(result) {
+    return resultToJson(result);
+  });
+}
+
+app.route('/resources/:id')
+  .get(function(request, response) {
+    getResourceById(request.params.id).then(function(result) {
+      response.send(result);
+    }).catch(function(error) {
+      if (error instanceof elasticsearch.errors.NotFound) {
+        response.sendStatus(404);
+      } else {
+        response.sendStatus(500);
+      }
+    });
+  })
+  .delete(function(request, response) {
+    client.delete({
+      index: 'myindex',
+      type: 'resources',
+      id: request.params.id
+    }).then(function(result) {
+      response.sendStatus(204);
+    }).catch(function(error) {
+      if (error instanceof elasticsearch.errors.NotFound) {
+        response.sendStatus(404);
+      } else {
+        response.sendStatus(500);
+      }
+    });
+  })
+  .put(function(request, response) {
+    // NOTE: this is a partial update
+    client.update({
+      index: 'myindex',
+      type: 'resources',
+      id: request.params.id,
+      body: {doc: request.body}
+    }).then(function(result) {
+      return getResourceById(result._id).then(function(object) {
+        response.send(object);
+      });
+    }).catch(function(error) {
+      if (error instanceof elasticsearch.errors.NotFound) {
+        response.sendStatus(404);
+      } else {
+        response.sendStatus(500);
+      }
+    });
+  });
+
+
+client.ping({requestTimeout: 3000, hello: 'hey'}).then(
+  function() {
+    var server = app.listen(3000, function() {
+      var host = server.address().address;
+      var port = server.address().port;
+
+      console.log(' [*] Listening at http://%s:%s', host, port);
+    });
+  },
+  function(err) {
+    process.exit(1);
+  }
+);
