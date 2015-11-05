0642471a769a6d4b9a4d95a1941e6fe2128dca89
s03y2015 <s03y2015@gg.ll>
2015-11-05 09:20:25 +0200

    d

--- testElasticLoad.js
+++ testElasticLoad.js
@@ -3,10 +3,30 @@ var csv = require("fast-csv");
 var elasticsearch = require('elasticsearch');
 
 var client = new elasticsearch.Client({ host: 'localhost:9200', log: 'trace', apiVersion: '2.0' });
-
+/*
 client.search({
-            index:'tada10',
+            index:'tada11',
             type:'cities',
-            q:'city:berlin' }).then(function(resources)
+            q:'city:burlin' }).then(function(resources)
             {console.log('Found  object:',resources.hits.hits);})
+
+*/
+
+
+ client.search(
+ 	{
+ 	index: 'tada11', type: 'cities',     
+    	body: { 
+            query: {
+        	    	match:{
+            	        	  city: {
+                	                  query: "burlin",
+                                           "fuzziness": 20,
+      									   "prefix_length": 1
+                                          }              
+                          }         
+                   }   
+              }  
+    }
+);  
             
